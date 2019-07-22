3f0524fa3d40394ed9c93b939d023734b10e2db5
Minh <aoeminh@gmail.com>
2019-07-22 17:32:52 +0700

    add menu when click fab

--- app/src/main/java/com/example/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -6,42 +6,58 @@ import android.support.design.widget.CoordinatorLayout
 import android.support.v4.app.Fragment
 import android.view.View
 import android.widget.RelativeLayout
-import com.example.myapplication.R
 import com.example.myapplication.behavior.BottomNavigationBehavior
 import com.example.myapplication.fragment.MessageFragment
 import com.example.myapplication.fragment.NotificationFragment
 import com.example.myapplication.fragment.TimelineFragment
 import com.example.myapplication.fragment.TopFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_fab.*
+import android.animation.Animator
+import android.R
+
+
 
 class MainActivity : AppCompatActivity() {
+    var isFABOpen = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.myapplication.R.layout.activity_main)
         initBottomMenu();
+
+        fab.setOnClickListener {
+            if (!isFABOpen) {
+                showFABMenu()
+            } else {
+                closeFABMenu()
+            }
+        }
+
+        fabBGLayout.setOnClickListener { closeFABMenu() }
     }
 
     fun initBottomMenu(){
-        bot_navi_main.selectedItemId = R.id.item_top
+        bot_navi_main.selectedItemId = com.example.myapplication.R.id.item_top
         loadFragment(TopFragment.newInstance())
         val layoutParamBotNavigation =  bot_navi_main.layoutParams as CoordinatorLayout.LayoutParams
         layoutParamBotNavigation.behavior = BottomNavigationBehavior()
         bot_navi_main.setOnNavigationItemSelectedListener { menuItem ->
             when(menuItem.itemId){
-                R.id.item_top -> {
+                com.example.myapplication.R.id.item_top -> {
                     loadFragment(TopFragment.newInstance())
                     showFab()
                 }
-                R.id.item_message -> {
+                com.example.myapplication.R.id.item_message -> {
                     hideFab()
                     loadFragment(MessageFragment.newInstance())
                 }
-                R.id.item_noti->{
+                com.example.myapplication.R.id.item_noti->{
                     hideFab()
                     loadFragment(NotificationFragment.newInstance())
                 }
-                R.id.item_timeline ->{
+                com.example.myapplication.R.id.item_timeline ->{
                     hideFab()
                     loadFragment(TimelineFragment.newInstance())
                 }
@@ -51,6 +67,59 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun showFABMenu() {
+        isFABOpen = true
+        fabLayout1.visibility = View.VISIBLE
+        fabLayout2.visibility = View.VISIBLE
+        fabLayout3.visibility = View.VISIBLE
+        fabBGLayout.setVisibility(View.VISIBLE)
+        fab.animate().rotationBy(180F)
+        fabLayout1.animate().translationY(-resources.getDimension(com.example.myapplication.R.dimen.standard_55))
+        fabLayout2.animate().translationY(-resources.getDimension(com.example.myapplication.R.dimen.standard_100))
+        fabLayout3.animate().translationY(-resources.getDimension(com.example.myapplication.R.dimen.standard_145))
+    }
+
+    private fun closeFABMenu() {
+        isFABOpen = false
+        fabBGLayout.setVisibility(View.GONE)
+        fab.animate().rotation(0f)
+        fabLayout1.animate().translationY(0f)
+        fabLayout2.animate().translationY(0f)
+        fabLayout3.animate().translationY(0f)
+        fabLayout3.animate().translationY(0f).setListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animator: Animator) {
+
+            }
+
+            override fun onAnimationEnd(animator: Animator) {
+                if (!isFABOpen) {
+                    fabLayout1.visibility = View.GONE
+                    fabLayout2.visibility = View.GONE
+                    fabLayout3.visibility = View.GONE
+                }
+                /*                if (fab.getRotation() != -180) {
+                    fab.setRotation(-180);
+                }*/
+            }
+
+            override fun onAnimationCancel(animator: Animator) {
+
+            }
+
+            override fun onAnimationRepeat(animator: Animator) {
+
+            }
+        })
+    }
+
+    override fun onBackPressed() {
+        if (isFABOpen) {
+            closeFABMenu()
+        } else {
+            super.onBackPressed()
+        }
     }
 
     fun hideFab(){
@@ -70,7 +139,7 @@ class MainActivity : AppCompatActivity() {
     fun loadFragment(fragment: Fragment){
         val fragmentManager = supportFragmentManager
         val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.frame_main,fragment)
+        transaction.replace(com.example.myapplication.R.id.frame_main,fragment)
         transaction.commit()
     }
 }
